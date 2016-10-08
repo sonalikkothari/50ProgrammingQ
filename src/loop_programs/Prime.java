5a5367c6fd44f046a47f7bc99a4fe7099947bad6
sonalikkothari <sonalikkothari@gmail.com>
2016-10-08 12:39:35 -0700

    Prime number program is added

---
+++ src/loop_programs/Prime.java
@@ -0,0 +1,34 @@
+package loop_programs;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Prime findprime = new Prime();
+		findprime.cal_prime_no(5);
+		findprime.cal_prime_no(12);
+		findprime.cal_prime_no(7);
+		findprime.cal_prime_no(66);
+		findprime.cal_prime_no(11);
+		findprime.cal_prime_no(13);
+		findprime.cal_prime_no(34);
+		findprime.cal_prime_no(61);
+	}
+
+	
+	public void cal_prime_no(int num) {
+		
+		int prime = 0;
+		for(int i=1; i<=num; i++) {
+			
+			if(num % i == 0) {
+				prime += 1;
+			}
+		}
+		if(prime == 2) {
+			System.out.println("The number " + num + " is a prime number");
+		} else {
+			System.out.println("The number " +num+" is not a prime no");
+		}
+	}
+}

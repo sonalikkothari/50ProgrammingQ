2f9b8dc49997df6775f2125350818f259c0f3381
sonalikkothari <sonalikkothari@gmail.com>
2016-10-07 22:34:41 -0700

    Display ping pong program added

---
+++ src/loop_programs/Display_Ping_pong.java
@@ -0,0 +1,28 @@
+package loop_programs;
+
+public class Display_Ping_pong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Display_Ping_pong dsply = new Display_Ping_pong();
+		dsply.printpingpong(15);
+	}
+
+	public void printpingpong(int num) {
+		
+		if(num %3 == 0 && num %5 == 0 ) {
+			System.out.println("The Entered number is divisible by 3 & 5 : PING-PONG");	
+			
+				
+		} else if(num % 3 == 0) {
+			System.out.println("The Entered number is divisible by 3: PING");
+				
+		} else if(num % 5 == 0) {
+			System.out.println("The Entered number is divisible by 5: PONG");
+			
+		}  else {
+			System.out.println("The Entered number is :"  + num);
+		}
+	}
+}

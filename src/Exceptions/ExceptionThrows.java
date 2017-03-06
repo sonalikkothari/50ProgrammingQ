1d85f4732d0ad1f4a3283f9fe8a562734dd48472
sonalikkothari <sonalikkothari@gmail.com>
2017-03-05 20:08:30 -0800

    First commit for Exceptions

---
+++ src/Exceptions/ExceptionThrows.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class ExceptionThrows {
+
+	public int arith_Except() throws ArithmeticException  {
+		int a = 20;
+		a = 20/0;
+		return a;
+		
+	}
+	
+}
---
+++ src/Exceptions/NullPointerExcep.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class NullPointerExcep {
+		
+	public void printWbl() {
+		System.out.println("Welcome to Whitebox-Learning");
+	}
+}

321922bb4dae3e8c86616377b93e493976d7bccb
sonalikkothari <sonalikkothari@gmail.com>
2017-03-05 20:19:12 -0800

    commit 1st

---
+++ src/Test/Test_Exceptions.java
@@ -0,0 +1,38 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+import Exceptions.ExceptionThrows;
+import Exceptions.NullPointerExcep;
+
+public class Test_Exceptions {
+
+	@Test
+	public void test_Null_Pointer_Excep() {
+		NullPointerExcep npExcp = null;
+		try {
+		npExcp.printWbl();
+	}
+		catch (NullPointerException nullexec) {
+			System.out.println("The exception is:: " +nullexec);
+		}
+		finally {
+			System.out.println("The Null Pointer Exception is handled");
+		}
+}
+
+	@Test
+	public void test_Thows_Exception() {
+		ExceptionThrows excpThrow = new ExceptionThrows();
+		try {
+		excpThrow.arith_Except();
+		}
+			catch( ArithmeticException arth) {
+				System.out.println("The arithmetic Exception:: " +arth);
+			} 
+		finally {
+			System.out.println("The Arethmetic Exception is handled..");
+		}
+	}
+
+}
